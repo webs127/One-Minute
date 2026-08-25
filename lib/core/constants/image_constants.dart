a9ac7f873c4b4260c974e60062536b84031d8cee
webs127 <davidshittu20@gmail.com>
2026-08-25 01:18:35 +0100

    feat: App icon integration

--- lib/core/constants/image_constants.dart
+++ lib/core/constants/image_constants.dart
@@ -1,6 +1,8 @@
 const String _base = "assets/images/";
+const String _icon = "assets/icon/";
 class ImageConstants {
   static const String onboarding1 = "${_base}onboarding1.jpg";
   static const String onboarding2 = "${_base}onboarding2.jpg";
   static const String onboarding3 = "${_base}onboarding3.jpg";
+  static const String icon = "${_icon}icon.png";
 }
